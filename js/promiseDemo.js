ca22cb2be05698eebd6b188f03a4ccfa58caaab4
mangdehuang <1032036512@qq.com>
2017-10-31 11:02:27 +0800

    promise step1

---
+++ js/promiseDemo.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Administrator on 2017/10/31.
+ * promise简单使用
+ */
+(function () {
+    let pics = ['pic.jpg','pic.jpg'];
+    var asyImg = function (url) {
+        let p = new Promise((res,rej) => {
+            const pic = new Image();
+            pic.src = url;
+            pic.onload = () => res(pic);
+            pic.onerror = () => rej();
+            console.log('333');
+        });
+        p.then( res => {
+            document.body.appendChild(res);
+            console.log('234');
+            },
+            rej => console.log('error')
+        );
+        console.log('456');
+        return p;
+    };
+    asyImg(pics[0]);
+})();
+/*
+   333
+   456
+   234
+ */
