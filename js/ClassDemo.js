745786e6e9321ec3983d0ea7af5dbc6dec562e20
mangdehuang <zhuerwei3320213>
2017-11-15 12:02:53 +0800

    class demo

--- js/ClassDemo.js
+++ js/ClassDemo.js
@@ -58,4 +58,80 @@
     }
 
     new B().foo('test');
+
+    class MyClass1{
+        set a(value) {
+            // this.a = value;
+        }
+
+        get a(){
+            // return this.a;
+        }
+    }
+
+    let my = new MyClass1();
+    my.a = '111';
+    console.log(my.a);
+
+    class MyClass2{
+        static p(){
+            console.log('test');
+        }
+    }
+    MyClass2.p();
+    // (new MyClass2()).p();
+
+    class Myclass3{
+        // prop1 = 1;//不可用
+        // static sprop1 = 2;
+        constructor(){
+            this.prop2 = 2;
+            console.log(this.prop1);
+        }
+    }
+    Myclass3.prop3 = 2;
+
+    var o1 = new Myclass3();
+    // console.log(o1.prop1);
+    console.log(o1.prop2);
+    console.log(Myclass3.prop3);
+    // console.log(Myclass3.sprop1);
+
+    class MyClass {
+         // myProp = {};
+
+        constructor() {
+            console.log(this.myProp); // 42
+        }
+    }
+    new MyClass();
+
+
+    function p1(name) {
+        if(new.target === p1){
+            this.name = name;
+        }else{
+            console.log('else');
+        }
+    }
+
+    var p11 = new p1('111');
+    p1.call(p11,'222');
+
+
+    class Myclass4{
+        constructor(name){
+            console.log(new.target === Myclass4);
+        }
+    }
+
+    var m4 = new Myclass4('11');
+
+    class Myclass5{
+        constructor(){
+            if(new.target !== Myclass5){
+
+            }
+        }
+    }
 })();
